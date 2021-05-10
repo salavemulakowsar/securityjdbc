6e05647b613e2ad2e1b0ee81f7dbc441843cc1e9
kowsar <salavemulak@gmail.com>
2021-05-10 16:11:09 +0530

    initial commit

---
+++ Springsecurityjdbc/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.jdbcAuthentication()
+			.dataSource(dataSource) //auto configure h2 database
+			.withDefaultSchema() //our h2 db will have couple of tables
+			.withUser(User.withUsername("kowsar")
+					.password("salavemula")
+					.roles("USER"))
+			.withUser(User.withUsername("k")
+					.password("s")
+					.roles("ADMIN"));
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")//please allow access to /user url to both user and admin
+		.antMatchers("/").permitAll()
+		//.hasAnyRole("_","_")
+		.and()
+		.formLogin();
+	}
+	
+}
